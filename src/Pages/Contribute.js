1ead20d585545cae055bc5c659ec4ea1bb1e100e
JunHao-k <koh.junhao@trentglobal.edu.sg>
2022-07-16 14:42:39 +0000

    Did some stuff

--- src/Pages/Contribute.js
+++ src/Pages/Contribute.js
@@ -9,7 +9,22 @@ export default class Contribute extends React.Component {
 
     state = {
         countryData: [],
-        tagsData: []
+        tagsData: [],
+        type: "",
+        name: "",
+        author: "",
+        description1: "",
+        description2: "",
+        description3: "",
+        country: "",
+        city: "",
+        email: "",
+        article: "",
+        ratings: null,
+        price: null,
+        stars: null,
+        tags_id: [],
+        image_url: ""
     }
 
     url = "https://8888-junhaok-wanderlustbe-fwpudmsqmrm.ws-us54.gitpod.io/"
@@ -22,8 +37,53 @@ export default class Contribute extends React.Component {
         })
     }
 
-    isEven = (num) => {
-        return (num%2===0)
+    // createListing = async () => {
+    //     let res = await axios.post(this.url + "contribute" , {
+    //         "type":,
+    //         "name":,
+    //         "author":,
+    //         "description1":,
+    //         "description2":,
+    //         "description3":,
+    //         "country":,
+    //         "city",
+    //         "email",
+    //         "article":,
+    //         "ratings":,
+    //         "price":,
+    //         "stars":,
+    //         "tags_id",
+    //         "image_url"
+    //     })
+    // }
+
+    /* 
+                let descriptionArr = [req.body.description1 , req.body.description2 , req.body.description3]
+
+        let type = req.body.type // Radiobuttons
+        let name = req.body.name // Input ==> Cannot be empty fields and must be string
+        let author = req.body.author // Input ==> Cannot be empty fields and must be string
+        let description = descriptionArr // Array of 3 String inputs ==> Cannot be empty fields and must be string
+        let country = ObjectId(req.body.country) // Dropdown selection 
+
+        // Cannot do anything to verify if the city name is legit anot can only validate for empty field and string
+        // Might have to use weather api or geocoding or whatever API to check if city is valid
+        let city = haveCity.length === 0 ? req.body.city : haveCity[0]._id 
+        let email = req.body.email // Must have @ and "." and is a string
+        let article = req.body.article 
+        let ratings = req.body.ratings // Maybe use a range slider 
+        let price = req.body.price // Must be float/int ==> Validate for this 
+        let stars = req.body.stars // Use Range slider also?
+        let tags = tagsCopy // Checkboxes ==> checkbox array cannot be empty
+        let image_url = req.body.image_url 
+        let reviews = []
+
+    */
+
+    updateFormField = (event) => {
+        this.setState({
+            [event.target.name]: event.target.value
+        })
     }
    
 
@@ -35,38 +95,38 @@ export default class Contribute extends React.Component {
                     <Form.Group className="p-3">
                         <Form.Label>Type</Form.Label>
                         <Form.Group className="mb-1 p-2" controlId="formBasicCheckbox">
-                            <Form.Check type="radio" label="Attraction" name="type" value="country" /*checked={this.props.searchLocation === "country"} onChange={() => this.props.updateQuery("country")} *//>
+                            <Form.Check type="radio" label="Attraction" name="type" value="Attraction" checked={this.state.type === "Attraction"} onChange={this.updateFormField}/>
                         </Form.Group>
                         <Form.Group className="mb-1 p-2" controlId="formBasicCheckbox">
-                            <Form.Check type="radio" label="Food" name="type" value="city" /*checked={this.props.searchLocation === "city"} onChange={() => this.props.updateQuery("city")} *//>
+                            <Form.Check type="radio" label="Food" name="type" value="Food" checked={this.state.type === "Food"} onChange={this.updateFormField}/>
                         </Form.Group>
                         <Form.Group className="mb-1 p-2" controlId="formBasicCheckbox">
-                            <Form.Check type="radio" label="Activity" name="type" value="city" /*checked={this.props.searchLocation === "city"} onChange={() => this.props.updateQuery("city")} *//>
+                            <Form.Check type="radio" label="Activity" name="type" value="Activity" checked={this.state.type === "Activity"} onChange={this.updateFormField}/>
                         </Form.Group>
                     </Form.Group>
 
 
                     <Form.Group className="mb-3 p-3" controlId="formPlaceName">
                         <Form.Label>Enter the name of attraction</Form.Label>
-                        <Form.Control type="text" placeholder="Place Name" />
+                        <Form.Control type="text" placeholder="Place Name" name = "name" value = {this.state.name} onCHange = {this.updateFormField}/>
                     </Form.Group>
 
                     <Form.Group className="mb-3 p-3" controlId="formAuthorName">
                         <Form.Label>Your Name</Form.Label>
-                        <Form.Control type="text" placeholder="Input Name" />
+                        <Form.Control type="text" placeholder="Input Name" name = "author" value = {this.state.author} onCHange = {this.updateFormField}/>
                     </Form.Group>
 
-
+                   
                     <Form.Group className="mb-3 p-3 g-3" controlId="formAuthorName">
                         <Form.Label>Short description of your recommendation</Form.Label>
                         <Form.Group className="p-1">
-                            <Form.Control type="text" placeholder="1. First description" />
+                            <Form.Control type="text" placeholder="1. First description" name = "description1" value = {this.state.description1} onCHange = {this.updateFormField}/>
                         </Form.Group>
                         <Form.Group className="p-1">
-                            <Form.Control type="text" placeholder="2. Second description" />
+                            <Form.Control type="text" placeholder="2. Second description" name = "description2" value = {this.state.description2} onCHange = {this.updateFormField}/>
                         </Form.Group>
                         <Form.Group className="p-1">
-                            <Form.Control type="text" placeholder="3. Third description" />
+                            <Form.Control type="text" placeholder="3. Third description" name = "description3" value = {this.state.description3} onCHange = {this.updateFormField}/>
                         </Form.Group>
                     </Form.Group>
 
