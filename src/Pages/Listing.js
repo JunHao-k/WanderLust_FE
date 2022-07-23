e1aa6243362cfde9bdc452877d9382ff3241a17c
JunHao-k <koh.junhao@trentglobal.edu.sg>
2022-07-23 20:09:20 +0000

    Tried outputting tags

--- src/Pages/Listing.js
+++ src/Pages/Listing.js
@@ -2,11 +2,17 @@ import React from "react";
 import Card from 'react-bootstrap/Card';
 import Col from 'react-bootstrap/Col';
 import Row from 'react-bootstrap/Row';
+import ListGroup from 'react-bootstrap/ListGroup';
+import Badge from 'react-bootstrap/Badge';
 // import Modal from 'react-bootstrap/Modal';
 // import Button from 'react-bootstrap/Button';
 // import Form from 'react-bootstrap/Form';
 import '../css/Listing.css'
 import axios from 'axios';
+import { RiStarSFill } from "react-icons/ri";
+import { FaDollarSign } from "react-icons/fa";
+
+
 
 export default class Listing extends React.Component {
 
@@ -16,6 +22,7 @@ export default class Listing extends React.Component {
     state = {
         data: [],
         tagsData: [],
+        tagLength: 0,
         query: this.props.query,
         place: this.props.place,
         filter: this.props.filter,
@@ -30,23 +37,24 @@ export default class Listing extends React.Component {
                 haveData: true
             })
         }
-        else if(this.state.filter === ""){
+        else if (this.state.filter === "") {
             let response = await axios.get(this.url + `?${this.state.query}=${this.state.place}`)
             this.setState({
                 data: response.data,
                 haveData: true
             })
         }
-        else if(this.state.filter === "best-rated"){
+        else if (this.state.filter === "best-rated") {
             let response = await axios.get(this.url + `/stars?${this.state.query}=${this.state.place}`)
             this.setState({
                 data: response.data,
                 haveData: true
             })
         }
-        else{
+        else {
             let tagsResponse = await axios.get(this.resUrl + "tags")
             let response = await axios.get(this.url + `/tags/${this.state.filter}?${this.state.query}=${this.state.place}`)
+    
             this.setState({
                 tagsData: tagsResponse.data,
                 data: response.data,
@@ -55,35 +63,60 @@ export default class Listing extends React.Component {
             })
         }
     }
-
+    renderTags = (result) => {
+        for(let eachObj of result){
+            this.setState({
+                tags: eachObj.tags_id
+            })
+        }
+    }
     renderContent = () => {
+        
         if (this.state.data.length !== 0) {
+            
             return (
                 <div >
                     <Row xs={1} md={2} lg={3} className="g-4 container-fluid" >
                         {Array.from({ length: this.state.data.length }).map((_, idx) => (
+                            
                             <React.Fragment key={this.state.data[idx]._id}>
                                 <Col className="card-holder">
-                                    <Card style={{ width: '18rem' }} onClick = {() => {
-                                            this.props.setListingId(this.state.data[idx]._id)
-                                            this.props.setActive("listing-details")
-                                        }}>
+                                    <Card style={{ width: '18rem' }} onClick={() => {
+                                        this.props.setListingId(this.state.data[idx]._id)
+                                        this.props.setActive("listing-details")
+                                    }}>
                                         <Card.Img variant="top" src={this.state.data[idx].images} />
                                         <Card.Body>
-                                            <Card.Title>{this.state.data[idx].name}</Card.Title>
-                                            <Card.Title>Author: {this.state.data[idx].author}</Card.Title>
-                                            <Card.Title>{this.state.data[idx].country} - {this.state.data[idx].city}</Card.Title>
-                                            <h6>Type: {this.state.data[idx].type}</h6>
-                                            <ul>
-                                                <li>Price: {this.state.data[idx].price !== 0 ? this.state.data[idx].price : "Free"}</li>
-                                                <li>Rated: {this.state.data[idx].ratings}</li>
-                                                <li>Number of stars: {this.state.data[idx].stars}</li>
-                                            </ul>
-                                            {/* <ul>
-                                                <li>{this.state.data[idx].description[0]}</li>
-                                                <li>{this.state.data[idx].description[1]}</li>
-                                                <li>{this.state.data[idx].description[2]}</li>
-                                            </ul> */}
+                                            <div className="card-info">
+                                                <Card.Title>{this.state.data[idx].name}</Card.Title>
+                                                <Card.Title><span>{this.state.data[idx].country} - {this.state.data[idx].city}</span></Card.Title>
+                                                <Card.Title>Author: <span>{this.state.data[idx].author}</span></Card.Title>
+                                                <Card.Title>Type: <span>{this.state.data[idx].type}</span></Card.Title>
+                                                <ListGroup variant="flush" className="card-list-group">
+                                                    <ListGroup.Item>Price: <span>{this.state.data[idx].price !== 0 ? this.state.data[idx].price : "Free"}</span></ListGroup.Item>
+                                                    <ListGroup.Item>Score: <span>{this.state.data[idx].ratings}/10</span></ListGroup.Item>
+                                                    <ListGroup.Item>Rated: <span>{Array.from({ length: this.state.data[idx].stars }).map((_, idx) => (
+                                                        <RiStarSFill color="#ffbb33" />
+                                                    ))}</span></ListGroup.Item>
+
+                                                </ListGroup>
+
+                                                {/* <div>
+                                                    {Array.from({ length: this.state.data[idx].tags_id.length }).map((_, idx) => (
+                                                        <Badge bg="secondary">{this.state.data[idx].tags_id[idx]}</Badge>
+                                                    ))}
+                                                </div> */}
+
+                                            </div>
+
+
+                                            {/* <h6>Price: {this.state.data[idx].price !== 0 ? this.state.data[idx].price : "Free"}</h6>
+                                            <h6>Score: {this.state.data[idx].ratings}/10</h6>
+                                            <h6>Rated: {Array.from({ length: this.state.data[idx].stars }).map((_, idx) => (
+                                                <RiStarSFill color="#ffbb33" />
+                                            ))}</h6> */}
+
+
                                             <Card.Footer className="text-muted">Click to see more details</Card.Footer>
                                         </Card.Body>
                                     </Card>
